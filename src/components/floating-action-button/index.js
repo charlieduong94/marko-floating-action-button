b31db3e7d710152373a980332477fd3453f55efa
Charlie Duong <charlieduong94@gmail.com>
2016-06-13 13:32:11 -0400

    Added the option to allow the component to be draggable

--- src/components/floating-action-button/index.js
+++ src/components/floating-action-button/index.js
@@ -1,5 +1,62 @@
 require('./styles/styles.less');
 
+function _getDocumentWidth() {
+    var width = window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+    return width;
+}
+
+function _getDocumentHeight() {
+    var height = window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+    return height;
+}
+
+
+function _startDrag(component, domEvent) {
+    var element = component.getEl();
+
+    document.addEventListener('mousemove', component.onMouseDrag);
+    document.addEventListener('mouseup', component.onMouseDragEnd);
+    component.setState('mouseDownTime', new Date().getTime());
+    component.setState('offsetY', element.offsetTop - domEvent.clientY);
+    component.setState('offsetX', element.offsetLeft - domEvent.clientX);
+    component.setState('dragged', true);
+    component.setState('dragging', true);
+    component.setState('x', domEvent.clientX);
+    component.setState('y', domEvent.clientY);
+}
+function _onDragEnd(component) {
+    return function (e) {
+        var time = new Date().getTime();
+        if(time - component.state.mouseDownTime > 250) {
+            console.log('mouse up');
+            setTimeout(function () {
+                component.setState('dragging', false);
+            }, 200);
+        } else {
+            component.setState('dragging', false);
+        }
+        console.log(component.state.dragging);
+        document.removeEventListener('mousemove', component.onMouseDrag);
+        document.removeEventListener('mouseup', component.onMouseDragEnd);
+
+    };
+}
+function _onDrag(component) {
+    return function (e) {
+        var x = e.clientX, y = e.clientY;
+        if (y < _getDocumentHeight() && y > 0) {
+            component.setState('y', e.clientY);
+        }
+        if (x < _getDocumentWidth() && x > 0){
+            component.setState('x', e.clientX);
+        }
+    };
+}
+
 function _showMenu(component) {
     clearInterval(component.revealTimeout);
     clearInterval(component.hideTimeout);
@@ -21,6 +78,19 @@ function _hideMenu(component) {
 module.exports = require('marko-widgets').defineComponent({
     template: require('./template.marko'),
 
+    init: function() {
+        var self = this;
+        var el = self.getEl();
+
+        self.onMouseDrag = _onDrag(self);
+        self.onMouseDragEnd = _onDragEnd(self);
+        self.onStartDrag = function(e) {
+            _startDrag(self, e);
+        };
+
+        el.addEventListener('mousedown', self.onStartDrag);
+    },
+
     handleMouseEnter: function() {
         _showMenu(this);
     },
@@ -30,10 +100,13 @@ module.exports = require('marko-widgets').defineComponent({
     },
 
     handleToggleReveal: function() {
-        if (this.state.reveal) {
-            _hideMenu(this);
-        } else {
-            _showMenu(this);
+        console.log(this.state.dragging);
+        if(!this.state.dragging) {
+            if (this.state.reveal) {
+                _hideMenu(this);
+            } else {
+                _showMenu(this);
+            }
         }
     },
 
@@ -62,7 +135,8 @@ module.exports = require('marko-widgets').defineComponent({
             menuItems: menuItems,
             revealOnHover: input.revealOnHover,
             direction: direction,
-            style: input.style
+            style: input.style,
+            draggable: input.draggable
         };
     },
 
@@ -75,7 +149,8 @@ module.exports = require('marko-widgets').defineComponent({
             revealOnHover: input.revealOnHover,
             direction: input.direction,
             reveal: false,
-            style: input.style
+            style: input.style,
+            draggable : input.draggable
         };
     },
 
