8583c97e377c39bcf0dfaeba4025a4a3152852f0
Charlie Duong <charlieduong94@gmail.com>
2016-06-14 09:42:13 -0400

    Added support for fixed components
    Also changed how menu buttons are positioned to better account for the directions
    they are rendered

--- src/components/floating-action-button/index.js
+++ src/components/floating-action-button/index.js
@@ -1,5 +1,12 @@
 require('./styles/styles.less');
 
+var buttonPlacements = [
+    'bottom-right',
+    'bottom-left',
+    'top-right',
+    'top-left'
+];
+
 function _getDocumentWidth() {
     var width = window.innerWidth ||
         document.documentElement.clientWidth ||
@@ -42,18 +49,34 @@ function _onDragEnd(component) {
         console.log(component.state.dragging);
         document.removeEventListener('mousemove', component.onMouseDrag);
         document.removeEventListener('mouseup', component.onMouseDragEnd);
-
+        setTimeout(function() {
+            component.setStateDirty('dragging', false);
+        }, 10);
     };
 }
 function _onDrag(component) {
     return function (e) {
+        console.log(e);
         var x = e.clientX, y = e.clientY;
-        if (y < _getDocumentHeight() && y > 0) {
-            component.setState('y', e.clientY);
+        var position = component.state.position;
+        if (!position || position.trim().toLowerCase() === 'absolute') {
+            var el = component.getEl();
+            var bounds = el.parentNode.getBoundingClientRect();
+            if (y < bounds.bottom && y > bounds.top) {
+                component.setState('y', y);
+            }
+            if (x < bounds.right && x > bounds.left){
+                component.setState('x', x);
+            }
+        } else {
+            if (y < _getDocumentHeight() && y > 0) {
+                component.setState('y', y);
+            }
+            if (x < _getDocumentWidth() && x > 0){
+                component.setState('x', x);
+            }
         }
-        if (x < _getDocumentWidth() && x > 0){
-            component.setState('x', e.clientX);
-        }
+
     };
 }
 
@@ -81,13 +104,11 @@ module.exports = require('marko-widgets').defineComponent({
     init: function() {
         var self = this;
         var el = self.getEl();
-
         self.onMouseDrag = _onDrag(self);
         self.onMouseDragEnd = _onDragEnd(self);
         self.onStartDrag = function(e) {
             _startDrag(self, e);
         };
-
         el.addEventListener('mousedown', self.onStartDrag);
     },
 
@@ -116,42 +137,48 @@ module.exports = require('marko-widgets').defineComponent({
         var mainButton = input['main-button'] || [];
         var menuItems = input['menu-item'] || [];
         var direction;
+        var center;
         switch(input.direction) {
             case 'left':
                 direction = 'marko-fa-row-reverse'
+                center = 'marko-fa-vertical-center';
                 break;
             case 'right':
                 direction = 'marko-fa-row';
+                center = 'marko-fa-vertical-center';
                 break;
             case 'down':
                 direction = 'marko-fa-column';
+                center = 'marko-fa-horizontal-center';
                 break;
             case 'up':
             default:
                 direction = 'marko-fa-column-reverse';
-         }
+                center = 'marko-fa-horizontal-center';
+        }
+        var placement = input.placement;
+        if (placement === undefined) {
+            placement = 'bottom-right';
+        } else if(buttonPlacements.indexOf(placement) === -1) {
+            throw new Error('Invalid button placement');
+        }
         return {
             mainButton: mainButton,
             menuItems: menuItems,
             revealOnHover: input.revealOnHover,
             direction: direction,
             style: input.style,
-            draggable: input.draggable
+            draggable: input.draggable,
+            placement: placement,
+            position: input.position,
+            center: center
         };
     },
 
     getInitialState: function(input) {
         console.log(input);
-        return {
-            mainButton: input.mainButton,
-            menuItems: input.menuItems,
-            showContent: false,
-            revealOnHover: input.revealOnHover,
-            direction: input.direction,
-            reveal: false,
-            style: input.style,
-            draggable : input.draggable
-        };
-    },
-
+        input.showContent = false;
+        input.reveal = false;
+        return input;
+    }
 });
